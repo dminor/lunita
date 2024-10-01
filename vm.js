faa6fe911b250727f77f63d1e9afd1f8ce6921f6
Daniel Minor <daniel.minor@gmail.com>
2024-10-01 18:59:33 -0400

    Add envs to virtual machine

--- vm.js
+++ vm.js
@@ -1,30 +1,84 @@
 export { Opcodes, VirtualMachine };
 
 const Opcodes = {
+  // Push the boolean false to the stack
+  // -> false
   FALSE: "false",
+  // Look up the value of identifier in the environment and push it to the stack
+  // id -> value
+  GETENV: "getenv",
+  // Look up the value of identifier in the global environment and push it to the stack
+  // id -> value
+  GLOBAL_SETENV: "global_setenv",
+  // Push an id to the stack
+  // -> identifier
   ID: "id",
+  // Pop two values from the stack, see if they are not equal
+  // a b -> a ~= b
   NEQ: "neq",
+  // Push a new table to the stack
+  // -> {}
   NEWTABLE: "newtable",
+  // Push a number to the stack
+  // -> number
   NUMBER: "number",
+  // Set the value of id in the environment to value
+  // id value ->
+  SETENV: "setenv",
+  // Push a string to the stack
+  // -> string
   STRING: "string",
+  // Push the boolean true to the stack
+  // -> true
   TRUE: "true",
 };
 
+// The Lua nil value
+const Nil = {};
+Nil.toString = function () {
+  return "nil";
+};
+
 class VirtualMachine {
+  env;
   stack;
   instructions;
   ip;
 
   constructor(instructions) {
+    this.env = [];
+    this.env.push(new Map()); // Global environment
     this.stack = [];
     this.instructions = instructions;
     this.ip = 0;
   }
 
+  lookupEnv(id) {}
+
   step() {
     switch (this.instructions[this.ip]) {
       case Opcodes.FALSE:
         this.stack.push(false);
+        break;
+      case Opcodes.GETENV:
+        {
+          const id = this.stack.pop();
+          let value = Nil;
+          for (let i = this.env.length - 1; i >= 0; i--) {
+            if (this.env[i].has(id)) {
+              value = this.env[i].get(id);
+              break;
+            }
+          }
+          this.stack.push(value);
+        }
+        break;
+      case Opcodes.GLOBAL_SETENV:
+        {
+          const value = this.stack.pop();
+          const id = this.stack.pop();
+          this.env[0].set(id, value);
+        }
         break;
       case Opcodes.ID:
         this.ip += 1;
@@ -41,6 +95,13 @@ class VirtualMachine {
       case Opcodes.NUMBER:
         this.ip += 1;
         this.stack.push(this.instructions[this.ip]);
+        break;
+      case Opcodes.SETENV:
+        {
+          const value = this.stack.pop();
+          const id = this.stack.pop();
+          this.env[this.env.length - 1].set(id, value);
+        }
         break;
       case Opcodes.STRING:
         this.ip += 1;
--- vm.test.js
+++ vm.test.js
@@ -51,3 +51,52 @@ test("neq", () => {
   expect(vm.stack.length).toBe(1);
   expect(vm.stack[0]).toBe(false);
 });
+
+test("env", () => {
+  let instr = [Opcodes.ID, "x", Opcodes.GETENV];
+  let vm = new VirtualMachine(instr);
+  vm.env[0].set("x", 1);
+  vm.run();
+  expect(vm.stack.length).toBe(1);
+  expect(vm.stack[0]).toBe(1);
+
+  instr = [Opcodes.ID, "x", Opcodes.GETENV];
+  vm = new VirtualMachine(instr);
+  vm.env[0].set("x", 1);
+  vm.env.push(new Map()); // Add a local environment
+  vm.run();
+  expect(vm.stack.length).toBe(1);
+  expect(vm.stack[0]).toBe(1);
+
+  instr = [Opcodes.ID, "x", Opcodes.NUMBER, 1, Opcodes.SETENV];
+  vm = new VirtualMachine(instr);
+  vm.run();
+  expect(vm.stack.length).toBe(0);
+  expect(vm.env[0].get("x")).toBe(1);
+
+  instr = [Opcodes.ID, "x", Opcodes.NUMBER, 1, Opcodes.GLOBAL_SETENV];
+  vm = new VirtualMachine(instr);
+  vm.env.push(new Map()); // Add a local environment
+  vm.run();
+  expect(vm.stack.length).toBe(0);
+  expect(vm.env[0].get("x")).toBe(1);
+
+  instr = [
+    Opcodes.ID,
+    "x",
+    Opcodes.NUMBER,
+    2,
+    Opcodes.SETENV,
+    Opcodes.ID,
+    "x",
+    Opcodes.NUMBER,
+    1,
+    Opcodes.GLOBAL_SETENV,
+  ];
+  vm = new VirtualMachine(instr);
+  vm.env.push(new Map()); // Add a local environment
+  vm.run();
+  expect(vm.stack.length).toBe(0);
+  expect(vm.env[1].get("x")).toBe(2);
+  expect(vm.env[0].get("x")).toBe(1);
+});
