69ebd1e6e11a5c038b840b06aeccd0a1da8850c5
Daniel Minor <daniel.minor@gmail.com>
2024-10-02 18:13:10 -0400

    Add interpreter

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import process from "node:process";
+
 import { parse } from "./parser.js";
 import { tokenize } from "./tokenizer.js";
+import { CodeGenerator } from "./codegen.js";
+import { VirtualMachine } from "./vm.js";
 
-const tokens = tokenize(program);
+const filename = process.argv[2];
+
+const handle = await fs.open(filename, "r");
+const program = await handle.readFile();
+handle.close();
+
+const tokens = tokenize(program.toString());
 const ast = parse(tokens);
+console.log(ast);
+const cg = new CodeGenerator();
+cg.generate(ast);
+const vm = new VirtualMachine(cg.instructions);
+vm.run();
